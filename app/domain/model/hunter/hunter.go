e967cc823ecf932e5111c3ab28c8ec2197cb8c1f
yu-croco <yuki.kita22@gmail.com>
2020-09-07 19:57:48 +0900

    add attack logic

--- app/domain/model/hunter/hunter.go
+++ app/domain/model/hunter/hunter.go
@@ -1,5 +1,10 @@
 package hunter
 
+import (
+	"errors"
+	"yu-croco/ddd_on_golang/app/domain/model/monster"
+)
+
 type Hunter struct {
 	Id Id `json:hunterId`
 	Name Name `json:hunterName`
@@ -16,3 +21,20 @@ type Life int
 type DefencePower int
 type OffensePower int
 type AttackDamage int
+
+func (hunter *Hunter) Attack(monster monster.Monster, damage AttackDamage) (*monster.Monster, error) {
+	return monster.AttackedBy(damage)
+}
+
+// ToDo: 条件を仕様クラスに切り出す
+func (hunter *Hunter) AttackedBy(givenDamage monster.AttackDamage) (*Hunter, error) {
+	var err error
+
+	if hunter.Life >= 0 {
+		hunter.Life = Life(givenDamage)
+	} else {
+		err = errors.New("すでに倒しています")
+	}
+
+	return hunter, err
+}
--- app/domain/model/monster/monster.go
+++ app/domain/model/monster/monster.go
@@ -1,5 +1,10 @@
 package monster
 
+import (
+	"errors"
+	"yu-croco/ddd_on_golang/app/domain/model/hunter"
+)
+
 type Monster struct {
 	Id           Id           `json:monsterId`
 	Name         Name         `json:monsterName`
@@ -16,3 +21,20 @@ type Life int
 type DefencePower int
 type OffensePower int
 type AttackDamage int
+
+func (monster *Monster) Attack(hunter hunter.Hunter, damage AttackDamage) (*hunter.Hunter, error) {
+	return hunter.AttackedBy(damage)
+}
+
+// ToDo: 条件を仕様クラスに切り出す
+func (monster *Monster) AttackedBy(givenDamage hunter.AttackDamage) (*Monster, error) {
+	var err error
+
+	if monster.Life >= 0 {
+		monster.Life = Life(givenDamage)
+	} else {
+		err = errors.New("すでに倒しています")
+	}
+
+	return monster, err
+}
